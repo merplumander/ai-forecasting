d9357ec6e2f39bbe716d84423ed59c95e386aeaf
Philipp Hummel <philipp.hummel1993@gmail.com>
2024-11-05 10:24:28 +0100

    Combine reasonings PoC

---
+++ test_combine_reasoning.py
@@ -0,0 +1,23 @@
+# %%
+
+question = "will bla be blu?"
+reasonings = ["bla", "blabla"]
+median_forecast = 80
+confidence_range_string = "[60%, 90%]"
+newline = "\n"
+reasonings = f"{newline.join(
+    f"Reasoning {number}:\n{reasoning}\n"
+    for number, reasoning in enumerate(reasonings))}"
+with open("combine_reasoning_prompt.txt", "r") as file:
+    # Read the entire content of the file
+    combine_prompt = file.read()
+
+combine_prompt = combine_prompt.format(
+    question=question,
+    reasonings=reasonings,
+    median_forecast=median_forecast,
+    confidence_range_string=confidence_range_string,
+)
+# %%
+print(combine_prompt)
+# %%
