dd070fc298ae1c85007cd48dbfd45a64d468c2a5
lreining <lars.reining@stud.tu-darmstadt.de>
2024-11-18 19:30:14 +0100

    Add comparison to metaculus forecasts

--- analyse_ensemble_metaculus.py
+++ analyse_ensemble_metaculus.py
@@ -2,11 +2,12 @@
 from pathlib import Path
 
 import numpy as np
+import pandas as pd
 import seaborn as sns
 from matplotlib import pyplot as plt
 
+from src.analysis.utils import brier_score, load_raw_forecasting_data
 from src.dataset.dataset import MetaculusDataset
-from src.forecasting.utils import brier_score, load_raw_forecasting_data
 
 ROOT = Path(__file__).parent
 # %%
@@ -56,21 +57,43 @@ sns.heatmap(corr_matrix, cbar_kws={"label": "Correlation"}, vmin=0.4, vmax=1)
 # %%
 resolutions = []
 ensemble_medians = []
-metaculus_predictions = []
-for question_id in median_matrix.index:
+max_days = 100
+metaculus_predictions_n_days_after_start = np.empty(
+    (len(median_matrix.index), max_days)
+)
+for i, question_id in enumerate(median_matrix.index):
     resolution = dataset.get_question(question_id).resolution
     resolutions.append(resolution)
     ensemble_median = forecasts[forecasts["question_id"] == question_id][
         "forecast"
     ].median()
-    ensemble_medians.append(ensemble_median)
-    latest = metaculus_prediction = eval(
+    metaculus_prediction_history = eval(
         dataset.questions.loc[question_id].aggregations
-    )["metaculus_prediction"]["latest"]
-    if latest is not None:
-        metaculus_predictions.append(latest["forecast_values"][1])
+    )["recency_weighted"]["history"]
+    if len(metaculus_prediction_history) > 0:
+        metaculus_prediction_df = pd.DataFrame(metaculus_prediction_history)
+        metaculus_prediction_df["start_time"] = pd.to_datetime(
+            metaculus_prediction_df["start_time"], unit="s"
+        )
+        metaculus_prediction_df["end_time"] = pd.to_datetime(
+            metaculus_prediction_df["end_time"], unit="s"
+        )
+        question_created_at = dataset.questions.loc[question_id].created_at
+        for j in range(0, max_days):
+            preds = metaculus_prediction_df[
+                (metaculus_prediction_df["end_time"] - question_created_at).abs()
+                <= pd.Timedelta(days=j + 1)
+            ]
+            if len(preds) > 0:
+                latest = preds.iloc[-1]
+            else:
+                latest = metaculus_prediction_df.iloc[3]
+            if latest["centers"] is not None:
+                metaculus_predictions_n_days_after_start[i, j] = latest["centers"][0]
+            else:
+                metaculus_predictions_n_days_after_start[i, j] = np.nan
     else:
-        metaculus_predictions.append(np.nan)
+        metaculus_predictions_n_days_after_start[i] = np.empty(max_days)
 
 
 brier_scores = median_matrix.apply(
@@ -81,6 +104,14 @@ brier_scores
 # brier score if we take median of ensemble as forecast
 brier_score(np.array(ensemble_medians) / 100, resolutions)
 # %%
-brier_score(np.array(metaculus_predictions) / 100, resolutions)
+brier_scores_days = []
+for j in range(max_days):
+    brier_scores_days.append(
+        brier_score(metaculus_predictions_n_days_after_start[:, j], resolutions)
+    )
+plt.plot(range(1, max_days + 1), brier_scores_days)
+plt.xlabel("Days after question start")
+plt.ylabel("Brier score")
+plt.legend(["Metaculus recency_weighted"])
 
 # %%
