0a08edd318a4e02c0bb0175ee4149473f68daff7
lreining <lars-reining@t-online.de>
2024-12-05 14:34:10 +0100

    Prompt builder with variable prompts

--- src/query/PromptBuilder.py
+++ src/query/PromptBuilder.py
@@ -1,7 +1,7 @@
 from abc import ABC, abstractmethod
 from datetime import datetime
 from pathlib import Path
-from typing import List
+from typing import List, override
 
 from newspaper import Article
 
@@ -22,15 +22,25 @@ class PromptBuilder(ABC):
         """Returns the user prompt for the specified question."""
         pass
 
+    def get_default_system_prompt_id() -> str:
+        """Returns the default system prompt id."""
+        return "sp_0"
+
 
 class BinaryQuestionWithDescriptionPromptBuilder(PromptBuilder):
 
-    def get_system_prompt():
-        with open(
-            ROOT / "prompts" / "binary_question_with_description_system_prompt.txt", "r"
-        ) as file:
+    def get_system_prompt(prompt_id: str = None):
+        if prompt_id is None:
+            prompt_id = (
+                BinaryQuestionWithDescriptionPromptBuilder.get_default_system_prompt_id()
+            )
+        with open(ROOT / "prompts" / "bqdsp" / f"{prompt_id}.txt", "r") as file:
             system_prompt = file.read()
-        return system_prompt
+        with open(
+            ROOT / "prompts" / "bqdsp" / "response_format_prompt.txt", "r"
+        ) as file:
+            format_prompt = file.read()
+        return "\n\n".join([system_prompt, format_prompt])
 
     def get_user_prompt(question: BinaryQuestion):
         with open(
@@ -43,6 +53,11 @@ class BinaryQuestionWithDescriptionPromptBuilder(PromptBuilder):
             today_date=datetime.today().strftime("%b %d, %Y"),
         )
         return user_prompt
+
+    @override
+    def get_default_system_prompt_id() -> str:
+        """Returns the default system prompt id."""
+        return "bqdsp_0"
 
 
 class BinaryQuestionWithDescriptionAndNewsPromptBuilder(
